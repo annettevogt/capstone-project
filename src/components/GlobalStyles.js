53b5f7339c458bcc5bf5a84256791f95a8b4b21b
mail@frauvogt.de <mail@frauvogt.de>
2020-07-03 16:37:02 +0200

    Finish Setup

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+:root{
+  --primary-dark: #011B1F;
+  --secondary: #344C54;
+  --secondary-40: #AEB7BB;
+  --primary-light: #C9B59E;
+  --primary-light-40: #E6DDD3;
+  --primary-light-8: #FBF9F8;
+  scroll-behavior: smooth;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: 'Montserrat', sans-serif;
+}
+
+body {
+  color: var(--primary-dark);
+  font-size: 18px;
+  font-weight: 300;
+  display: grid;
+  grid-template-rows: 48px auto 48px;
+  background: var(--primary-light-8);
+}
+
+main {
+  overflow: scroll;
+  line-height: 1.5;
+  display: grid;
+  padding: 72px 24px;
+  height: 100%;
+}
+
+header {
+  position: fixed;
+  z-index: 1;
+  height: 48px;
+}
+`
